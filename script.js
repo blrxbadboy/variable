d874d9d815c51bf6333e8251230d5a503deb6e21
blrxbadboy <rayenbounawara7@gmail.com>
2026-01-18 18:17:10 +0100

    correction

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+//ex 1
+var animal="monkey";
+var monkey=animal;
+var x=15;
+var y=10;
+var Name="y3ni?";
+var isTrue=false;
+var isTenEven=10%2===0;
+
+//ex 2
+
+var firstName = "med rayen"
+var lastName = "Bounawara"
+var fullName = firstName + " " + lastName;
+
+//ex 3
+var n1=0
+function counter() {
+n1=n1+1 
+  return n1
+}
+
+//ex 3
+
+var monAge
+console.log(monAge);
+monAge=21
+console.log(monAge);
+erreur identification
+
+
+// ex 4
+// amalt recherche kfh ntaalaa type mn ggl
+var nom = "med rayen bn"
+var estConnecte=true
+var nombre=21
+console.log(typeof nom);
+console.log(typeof estConnecte);
+console.log(typeof nombre);
+
+// ex 5
+ function sumTwoNumbers(n1,n2) {
+    var somme = n1+n2
+    return somme
+ }
+
+ //ex 6
+
+ function priceWithTax(prix,tax) {
+  var taxAm=(prix*tax)/100   //nos seea natrah fl prix m tax tal3t pourcentage
+  var total=prix+taxAm
+  return total
+}
+priceWithTax(100,15)
+priceWithTax(80,20)
+
 
